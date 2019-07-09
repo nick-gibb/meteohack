3a7c760bc0eaa90218201e73c091b467118a0b46
nick-gibb <gibbnick@gmail.com>
2019-07-09 09:29:28 -0400

    adding ac to form

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
+import pandas as pd
+filename = "data/010_room_air_conditioners_climatiseurs_individuels/010_Data_Données.csv"
+acs = pd.read_csv(filename, encoding='cp1252')
+brands = acs["BRAND_NAME"].unique().tolist()
+
+choices = [(g, g) for g in brands]
 
 
 class LoginForm(FlaskForm):
     postal = StringField('Postal code', validators=[DataRequired()])
+    air_con = SelectField('Air conditioner brand', validators=[
+                          DataRequired()], choices=choices)
+    model = SelectField('Model', validators=[
+                        DataRequired()], choices=[('', '')])
     submit = SubmitField('Submit')
